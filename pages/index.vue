7e4c169e0ac2e3f399649b6221097950e4c2c488
Panha <pospanha.ctg@gmail.com>
2024-05-08 08:30:56 +0700

    test api

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+       <h1>API Data</h1>
+       <div v-if="data">
+         <p>{{ data }}</p>
+       </div>
+       <div v-else>
+         <p>Loading...</p>
+       </div>
+    </div>
+   </template>
+   
+   <script>
+   export default {
+    data() {
+       return {
+         data: null,
+       };
+    },
+    async mounted() {
+       try {
+         const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+         if (!response.ok) {
+           throw new Error('Network response was not ok');
+         }
+         this.data = await response.json();
+       } catch (error) {
+         console.error('There was a problem with your fetch operation:', error);
+       }
+    },
+   };
+   </script>
